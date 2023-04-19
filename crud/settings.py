3b44f232d9e7f0170219c626f7b94677c977b8eb
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-04-19 14:33:48 +0900

    Add ALLOWED_HOSTS in settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-cloudtype-test-1maxx2algn9766g.sel3.cloudtype.app']
 
 
 # Application definition
